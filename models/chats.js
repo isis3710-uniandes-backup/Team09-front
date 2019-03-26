fc199c31ae255238f436a3f5e55af24bb9f91cb4
James Lake <jh.lake@uniandes.edu.co>
2019-03-26 18:33:16 -0500

    restructuring to separate back and front

--- models/chats.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Chats = sequelize.define('Chats', {
-    chatId: {type:DataTypes.INTEGER, primaryKey:true},
-    roomId: {type:DataTypes.INTEGER, references: 'rooms', referencesKey: 'roomId' }
-  }, {timestamps: false});
-
-  return Chats;
-};
--- models/groups.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Groups = sequelize.define('Groups', {
-    id: {type:DataTypes.INTEGER, primaryKey:true},
-    name: DataTypes.STRING
-  }, {timestamps: false});
-
-  return Groups;
-};
